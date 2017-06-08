5d76a131b3a2c304e7c9c7d8666ae48d4362b5af
Jiri Kuncar <jiri.kuncar@gmail.com>
2017-06-08 16:40:04 +0200

    Preprocess numbers in equations

--- essm/equations/_core.py
+++ essm/equations/_core.py
@@ -4,10 +4,14 @@
 
 from __future__ import absolute_import
 
+import ast
+import inspect
+import sys
 import warnings
 
 from sage.all import SR, Expression
 from sage.misc.latex import latex
+from sage.rings import integer, real_mpfr
 
 from ..variables import SHORT_UNIT_SYMBOLS, Variable
 
@@ -50,6 +54,65 @@ class BaseEquation(Expression):
     def convert(self):
         return convert(self)
 
+_Integer = ast.parse('integer.Integer', mode='eval').body
+_Float = ast.parse('real_mpfr.RR', mode='eval').body
+
+
+class Numbers(ast.NodeTransformer):
+    """Change Python numbers to Sage numbers."""
+
+    def visit_Num(self, node):
+        """Rewrite int / int to Fraction(int, int)."""
+        func = _Integer
+        if isinstance(node.n, float):
+            func = _Float
+        return ast.copy_location(ast.Call(
+            func=func,
+            args=[ast.Str(str(node.n))],
+            keywords=[],
+            starargs=None,
+            kwargs=None,
+        ), node)
+
+
+class ClassDef(ast.NodeVisitor):
+    """Extract expression definition."""
+
+    def __init__(self):
+        """Initialize class definition."""
+        self.expr = None
+
+    def visit_Assign(self, node):
+        """Find 'expr = <expr>'."""
+        for target in node.targets:
+            if target.id != 'expr':
+                continue
+            expr = ast.Expression(Numbers().visit(node.value))
+            ast.fix_missing_locations(expr)
+            self.expr = compile(expr, '<string>', mode='eval')
+            break
+
+
+def build_instance_expression(instance, back=1):
+    """Return fixed expression."""
+    # Evaluate expression in the original context.
+    frame = sys._getframe(back + 1)
+
+    # Find original code and convert numbers.
+    code = ast.parse(inspect.getsource(instance))
+    class_def = ClassDef()
+    class_def.visit(code)
+
+    # Include names used during number replacement.
+    f_globals = frame.f_globals.copy()
+    f_globals.setdefault('integer', integer)
+    f_globals.setdefault('real_mpfr', real_mpfr)
+
+    return BaseEquation(
+        SR, eval(class_def.expr, f_globals, frame.f_locals)
+    )
+
+
 
 class EquationMeta(type):
     """Equation interface."""
@@ -57,14 +120,12 @@ class EquationMeta(type):
     def __new__(cls, name, parents, dct):
         """Build and register new variable."""
         if '__registry__' not in dct:
-            name = dct.get('name', name)
-            expr = BaseEquation(SR, dct['expr'])
-            dct.update({
-                'name': name,
-                'expr': expr,
-            })
+            dct.setdefault('name', name)
+            expr = dct.pop('expr')
 
             instance = super(EquationMeta, cls).__new__(cls, name, parents, dct)
+            instance.expr = expr = build_instance_expression(instance)
+
             if expr in instance.__registry__:
                 warnings.warn(
                     'Equation "{0}" will be overridden by "{1}"'.format(
