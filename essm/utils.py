b6f2c351994939a7552d45a1df8f21f3b86b4bb2
Stan Schymanski <stan.schymanski@env.ethz.ch>
2017-06-09 17:10:55 +0200

    Fix instance test

--- essm/utils.py
+++ essm/utils.py
@@ -7,5 +7,5 @@ from __future__ import absolute_import
 def process_parents(parents, cls, attribute='definition'):
     """Process parents that subclass given class."""
     return tuple(
-        getattr(parent, attribute) if issubclass(parent, cls) else parent
+        getattr(parent, attribute) if isinstance(parent, cls) else parent
         for parent in parents)
