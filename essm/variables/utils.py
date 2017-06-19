9938cdc8c357e9ea9cd91a9e4609a79ab5720035
Jiri Kuncar <jiri.kuncar@gmail.com>
2017-06-19 16:59:39 +0200

    Added generator of variable metadata

---
+++ essm/variables/utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Utility function for variables."""
+
+from .units import markdown
+
+
+def generate_metadata_table(variables=None, include_header=True):
+    """Generate table of variables, default values and units.
+
+    If variables not provided in list ``variables``, table
+    will contain all variables in ``Variables.__registry__``.
+    """
+    variables = variables or Variable.__registry__.keys()
+    if include_header:
+        yield ('Symbol', 'Name', 'Description', 'Default value', 'Units')
+
+    for variable in sorted(variables, key=lambda x: x.latex_name):
+        symbol = '$' + variable.definition.latex + '$'
+        name = str(variable)
+        doc = variable.__doc__
+        val = str(variable.__defaults__.get(v1, '-')
+
+        yield (symbol, name, doc, val, markdown(variable.short_unit)))
