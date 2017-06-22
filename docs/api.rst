6f853f5994a9992dc43075c85331e4d8ad644600
Jiri Kuncar <jiri.kuncar@gmail.com>
2017-06-22 10:04:35 +0200

    docs: structure and new location

---
+++ docs/api.rst
@@ -0,0 +1,87 @@
+========
+API Docs
+========
+
+Variables
+=========
+
+.. automodule:: essm.variables
+
+Chamber
+-------
+
+.. automodule:: essm.variables.chamber
+
+Mass
+~~~~
+
+.. automodule:: essm.variables.chamber.mass
+   :members:
+
+Insulation
+~~~~~~~~~~
+
+.. automodule:: essm.variables.chamber.insulation
+   :members:
+
+Leaf
+----
+
+.. automodule:: essm.variables.leaf
+
+Radiation
+~~~~~~~~~
+
+.. automodule:: essm.variables.leaf.radiation
+   :members:
+
+Water Vapour
+~~~~~~~~~~~~
+
+.. automodule:: essm.variables.leaf.water_vapour
+   :members:
+
+Physics
+-------
+
+.. automodule:: essm.variables.physics.thermodynamics
+   :members:
+
+Units
+-----
+
+.. automodule:: essm.variables.units
+   :members:
+
+Equations
+=========
+
+.. automodule:: essm.equations
+
+Chamber
+-------
+
+.. automodule:: essm.equations.chamber
+
+Insulation
+~~~~~~~~~~
+
+.. automodule:: essm.equations.chamber.insulation
+   :members:
+
+Leaf
+----
+
+.. automodule:: essm.equations.leaf
+
+Energy Water
+~~~~~~~~~~~~
+
+.. automodule:: essm.equations.leaf.energy_water
+   :members:
+
+Physics
+-------
+
+.. automodule:: essm.equations.physics.thermodynamics
+   :members:
--- essm/__init__.py
+++ essm/__init__.py
@@ -17,7 +17,62 @@
 # along with essm; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA 02111-1307, USA.
-"""Global tools."""
+"""ESSM contains helpers to deal with physical variables and units.
+
+Creating variables
+==================
+To create custom variables, first import `Variable`:
+
+    >>> from essm.variables import Variable
+
+To define units, you can either import these units from the library:
+
+    >>> from essm.variables.units import joule, kelvin, meter
+
+Then you can define a custom variable with its name, description, domain,
+latex_name, and unit, e.g.:
+
+    >>> class demo_chamber_volume(Variable):
+    ...    '''Volume of chamber.'''
+    ...    latex_name = 'V_c'
+    ...    domain = 'real'
+    ...    name = 'V_c'
+    ...    unit = meter ** 3
+    ...    default = 1
+
+Now, `demo_chamber_volume` is displayed as `V_c` and you can type
+`help(demo_chamber_volume)`.
+
+`Variable.__defaults__` returns a dictionary with all variables and
+their default values, `Variable.__units__` returns their units, and
+`demo_chamber_volume.short_unit()` can be used to obtain the units
+in short notation.
+
+You can import pre-defined variables as e.g.:
+
+    >>> from essm.variables.chamber.insulation import *
+    >>> from essm.variables.chamber.mass import *
+    >>> from essm.variables.leaf.water_vapour import *
+
+Creating equations
+==================
+
+To create custom equations, proceed similarly to avove, i.e. first import
+`Equation`:
+
+    >>> from essm.equations import Equation
+
+Then define an equation, e.g.:
+
+    >>> class demo_eq_Qi(Equation):
+    ...    '''Calculate heat conduction through insulation material.
+    ...
+    ...    Uses :math:`Q_{i}` as a function of temperature difference
+    ...    :math:`dT_{i}`, and material properties.
+    ...    '''
+    ...    expr = Q_i == dT_i * lambda_i * A_i / L_i
+
+"""
 
 from __future__ import absolute_import
 
