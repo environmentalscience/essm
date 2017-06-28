f864281a5fc12b7fb2d57472a54174f68d9c5068
Stan Schymanski <stan.schymanski@env.ethz.ch>
2017-06-28 08:36:40 +0200

    Updated docs trings in __init__.py, but building documentation fails due to weird indent error.

--- essm/variables/__init__.py
+++ essm/variables/__init__.py
@@ -17,7 +17,51 @@
 # along with essm; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA 02111-1307, USA.
-"""Define useful variables and symbols."""
+"""Variables module to deal with physical variables and units.
+
+It allows attaching docstrings to variable names, defining their domains
+(e.g. integer, real or complex), their units and LaTeX representations.
+You can also provide a default value, which is particularly useful for
+physical constants.
+
+Creating variables
+==================
+To create custom variables, first import `Variable`:
+
+    >>> from essm.variables import Variable
+
+To define units, you must first import these units from the library:
+
+    >>> from essm.variables.units import joule, kelvin, meter
+
+Then you can define a custom variable with its name, description, domain,
+latex_name, unit, and an optional default value, e.g.:
+
+    >>> class demo_chamber_volume1(Variable):
+    ...    '''Volume of Chamber 1.'''
+    ...    latex_name = 'V_c1'
+    ...    domain = 'real'
+    ...    name = 'V_{c1}'
+    ...    unit = meter ** 3
+    ...    default = 1
+
+Now, demo_chamber_volume is displayed as V_c1 and it will be
+nicely rendered in LaTeX as :math:`V_{c1}`.
+
+You can type `help(demo_chamber_volume)` to inspects its metadata.
+
+`Variable.__defaults__` returns a dictionary with all variables and
+their default values, `Variable.__units__` returns their units, and
+`demo_chamber_volume.short_unit()` can be used to obtain the units
+in short notation.
+
+This module also contains libraries of pre-defined variables, which
+can be imported into your SageMath session, e.g.:
+     >>> from essm.variables.physics.thermodynamics import *
+     >>> from essm.variables.leaf.energy_water import *
+
+
+"""
 
 from __future__ import absolute_import
 
