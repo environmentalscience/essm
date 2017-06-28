578ac7c4bd1a2824a1d78fc32d89305b14238e5e
Stan Schymanski <stan.schymanski@env.ethz.ch>
2017-06-28 11:18:38 +0200

    Added isort to break long lines in EquationWriter and VariableWriter.

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -34,8 +34,8 @@ def test_equation():
     assert demo_fall.__doc__ == demo_fall.definition.__doc__
     assert solve(
         demo_fall.subs(Variable.__defaults__).subs(
-            demo_fall.definition.t == 1), demo_fall.definition.d) == [
-                demo_fall.definition.d == 4.9]
+            demo_fall.definition.t == 1),
+        demo_fall.definition.d) == [demo_fall.definition.d == 4.9]
 
 
 def test_units():
@@ -43,7 +43,6 @@ def test_units():
     with pytest.raises(ValueError):
 
         class invalid_units(Equation):
-
             class x(Variable):
                 unit = meter
 
@@ -55,14 +54,14 @@ def test_args():
     assert set(demo_fall.definition.args()) == {
         demo_g.definition,
         demo_fall.definition.d.definition,
-        demo_fall.definition.t.definition, }
+        demo_fall.definition.t.definition,
+    }
 
 
 def test_unit_check():
     """Check unit test involving temperature."""
 
     class combined_units(Equation):
-
         class x_mol(Variable):
             unit = joule / mole / kelvin
 
@@ -80,6 +79,7 @@ def test_unit_check():
 
 def test_double_registration():
     """Check double registration warning."""
+
     class demo_double(Equation):
         """First."""
 
@@ -88,6 +88,7 @@ def test_double_registration():
     assert Equation.__registry__[demo_double].__doc__ == 'First.'
 
     with pytest.warns(UserWarning):
+
         class demo_double(Equation):  # ignore: W0232
             """Second."""
 
@@ -104,17 +105,52 @@ def test_equation_writer(tmpdir):
     writer_td = EquationWriter(docstring='Test of Equation_writer.')
     writer_td.eq(
         'demo_fall',
-        demo_g == d / t ** 2,
+        demo_g == d / t**2,
         doc='Test equation.\n\n    (Some reference)\n    ',
         variables=[{
             "name": "d",
             "value": '0.9',
             "units": meter,
-            "latexname": 'p_1'}, {
-                "name": "t",
-                "units": second,
-                "latexname": 'p_2'}])
+            "latexname": 'p_1'
+        }, {
+            "name": "t",
+            "units": second,
+            "latexname": 'p_2'
+        }])
     eq_file = tmpdir.mkdir('test').join('test_equations.py')
     writer_td.write(eq_file.strpath)
     execfile(eq_file.strpath, g)
     assert g['demo_fall'].definition.d.definition.default == 0.9
+
+
+def test_equation_writer_linebreaks(tmpdir):
+    """EquationWriter breaks long import lines."""
+    contents = {}
+    from essm.variables.physics.thermodynamics import *
+    writer_td = EquationWriter(docstring='Test of Equation_writer.')
+    writer_td.eq(
+        'eq_Le',
+        Le == alpha_a / D_va,
+        doc='Le as function of alpha_a and D_va.')
+    writer_td.eq(
+        'eq_Cwa',
+        C_wa == P_wa / (R_mol * T_a),
+        doc='C_wa as a function of P_wa and T_a.')
+    writer_td.eq(
+        'eq_rhoa_Pwa_Ta',
+        rho_a == (M_w * P_wa + M_N2 * P_N2 + M_O2 * P_O2) / (R_mol * T_a),
+        doc='rho_a as a function of P_wa and T_a.')
+    writer_td.eq(
+        'eq_Pa',
+        P_a == P_N2 + P_O2 + P_wa,
+        doc='Calculate air pressure from partial pressures.')
+    writer_td.eq(
+        'eq_PN2_PO2',
+        P_N2 == x_N2 / x_O2 * P_O2,
+        doc='Calculate P_N2 as a function of P_O2')
+
+    eq_file = tmpdir.mkdir('test').join('test_equations.py')
+    writer_td.write(eq_file.strpath)
+    with open(eq_file.strpath) as outfile:
+        maxlinelength = max([len(line) for line in outfile.readlines()])
+    assert maxlinelength < 80
