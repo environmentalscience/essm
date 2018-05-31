7b72c0ab405c9b058001ab78b48a3319f6ad21bb
Stan Schymanski <stan.schymanski@env.ethz.ch>
2018-05-31 11:50:12 +0000

    docs: deployment to github pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '1.2.0.dev20180323',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
