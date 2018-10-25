e864a59aef710b8c47c7a86c08d748ea91db39c1
Jiri Kuncar <jiri.kuncar@gmail.com>
2018-10-25 13:28:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -94,6 +94,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Development Status :: 1 - Planning',
     ],
