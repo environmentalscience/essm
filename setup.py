581a0cb2cfb66fb792d57df0b18d118a65a041a8
Stan Schymanski <stan.schymanski@env.ethz.ch>
2018-10-26 15:42:34 +0200

    Remove Python 3.7 from setup.py and travis.yml to avoid travis error.

--- setup.py
+++ setup.py
@@ -96,7 +96,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Development Status :: 1 - Planning',
     ],
 )
