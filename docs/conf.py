e617013b9c3019785fa2113bd61a9b14950273c6
Jiri Kuncar <jiri.kuncar@gmail.com>
2019-04-09 00:40:10 +0200

    docs: include logo (#69)

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,6 @@ from essm.variables._core import BaseVariable
 sys.path.insert(0, os.path.abspath('..'))
 
 
-
-
 class BaseDocumenter(ClassDocumenter):
     """Document expression definitions."""
 
@@ -162,6 +160,8 @@ html_theme = 'alabaster'
 # documentation.
 #
 html_theme_options = {
+    'logo': 'logo.png',
+    'logo_name': False,
     'description':
         'This package contains helpers to deal with physical variables '
         'and units.',
@@ -179,7 +179,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = {
